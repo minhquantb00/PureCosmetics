90c249a9cef6f86bb8a99c10e6bc581b2dd39f8f
minhquantb00 <minhquantb00@gmail.com>
2025-12-15 21:15:33 +0700

    Ignore VS cache

--- PureCosmetics.GrpcServer/obj/Debug/net9.0/PureCosmetics.GrpcServer.AssemblyInfo.cs
+++ PureCosmetics.GrpcServer/obj/Debug/net9.0/PureCosmetics.GrpcServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PureCosmetics.GrpcServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bed2f7a6dd0209c880e4f42a5eb7275173f3dfe4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd6dcbcb843a8422c48a5fead8b459aefe7f8330")]
 [assembly: System.Reflection.AssemblyProductAttribute("PureCosmetics.GrpcServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PureCosmetics.GrpcServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
